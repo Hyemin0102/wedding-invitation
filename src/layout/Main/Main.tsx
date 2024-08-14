398630ec1bacc10b7a4a860d45e06723eaad3fc8
hyemin.jo <gkdlt7373@gmail.com>
2024-08-15 00:20:49 +0900

    이미지 경로 수정2

--- src/layout/Main/Main.tsx
+++ src/layout/Main/Main.tsx
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled';
 // import data from 'data.json';
-import mainImg from '@/assets/images/2-1.png';
+//import mainImg from '@/assets/images/2-1.png';
+import mainImg from '../../assets/images/2-1.png';
+
 
 const Main = () => {
   // const { greeting } = data;
