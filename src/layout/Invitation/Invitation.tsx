4d251173bd38b71b552a09f99fd83b0fd55f9acf
hyemin.jo <gkdlt7373@gmail.com>
2024-08-28 09:46:57 +0900

    미사용 파일 삭제

--- src/layout/Invitation/Invitation.tsx
+++ src/layout/Invitation/Invitation.tsx
@@ -10,20 +10,8 @@ const Invitation = () => {
     <InvitationWrapper>
       <div style={{marginBottom: '10px'}}>
         <img style={{width: '80%'}} src='/aa.png' alt="리본" />
-        <img style={{width: '95%'}} src='/invitation_3.png' alt="초대문구" />
+        <img style={{width: '95%'}} src='/invitation_4.png' alt="초대문구" />
       </div>
-      {/*<Paragraph>*/}
-      {/*  <div>*/}
-      {/*    곁에서 매일 재워주고 싶은 사람을 만났습니다.*/}
-      {/*  </div>*/}
-      {/*  <div>*/}
-      {/*    하루의 끝에서 매일 일상을 나누고 싶은 사람을 만났습니다.*/}
-      {/*  </div>*/}
-      {/*  <div style={{marginTop: "16px"}}>*/}
-      {/*    시간과 함께 낡아지는 것이 아니라 깊어지는 사랑을 평생 약속하려 합니다.*/}
-      {/*  </div>*/}
-      {/*  <div>귀한 걸음 함께해 주시면 행복한 마음으로 간직하겠습니다.</div>*/}
-      {/*</Paragraph>*/}
       <Host />
     </InvitationWrapper>
   );
